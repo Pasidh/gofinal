d28dafa7263dd261b50bafa3ff9a75a5d45fef84
Pasidh.k <69345930+Pasidh@users.noreply.github.com>
2020-08-29 17:42:36 +0700

    Add files via upload

--- main.go
+++ main.go
@@ -1,8 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"github.com/Pasidh/gofinal/middleware"
+	"github.com/Pasidh/gofinal/task"
+	"github.com/gin-gonic/gin"
+)
+
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+	r.Use(middleware.Auth)
+
+	r.GET("/customers", task.GetTodosHandler)
+	r.POST("/customers", task.CreateTodosHandler)
+	r.GET("/customers/:id", task.GetTodoByIdHandler)
+	r.PUT("/customers/:id", task.UpdateTodosHandler)
+	r.DELETE("/customers/:id", task.DeleteTodosHandler)
+
+	return r
+}
 
 func main() {
-	fmt.Println("customer service")
-	//run port ":2009"
+	task.Init()
+	task.CreateTable()
+	r := setupRouter()
+	r.Run(":2009")
 }
